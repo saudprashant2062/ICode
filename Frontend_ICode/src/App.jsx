59f6a93f8b39e13e30ed15488ea161debfae498f
Prashant Saud <saudprashant2062@gmail.com>
2026-06-17 18:18:31 +0530

    Added Pages

--- Frontend_ICode/src/App.jsx
+++ Frontend_ICode/src/App.jsx
@@ -1,10 +1,22 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+import Landing from "./pages/Landing";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
+import Dashboard from "./pages/Dashboard";
+import Editor from "./pages/Editor";
+
 function App() {
   return (
-    <div className="min-h-screen flex items-center justify-center">
-      <h1 className="text-4xl font-bold">
-        ICode
-      </h1>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Landing />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/editor" element={<Editor />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
---
+++ Frontend_ICode/src/pages/Landing.jsx
@@ -0,0 +1,8 @@
+export default function Landing() {
+  return (
+    <div>
+      <h1>Landing Page</h1>
+      <p>ICode is running</p>
+    </div>
+  );
+}
