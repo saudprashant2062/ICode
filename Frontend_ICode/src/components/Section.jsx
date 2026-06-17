29d41ce2d0789e3193d076dd635878240e992f51
Prashant Saud <saudprashant2062@gmail.com>
2026-06-17 19:42:39 +0530

    Changed UI and added componenets

---
+++ Frontend_ICode/src/components/Section.jsx
@@ -0,0 +1,7 @@
+export default function Section({ children }) {
+  return (
+    <section className="max-w-4xl mx-auto p-8">
+      {children}
+    </section>
+  );
+}
--- Frontend_ICode/src/pages/Landing.jsx
+++ Frontend_ICode/src/pages/Landing.jsx
@@ -1,10 +1,30 @@
 import Navbar from "../components/Navbar";
-
+import { Link } from "react-router-dom";
 export default function Landing() {
   return (
     <>
       <Navbar title="ICode" />
-      <h1>Landing Page</h1>
+      <main className="max-w-4xl mx-auto p-8 mt-32">
+  <div className="space-y-6 max-w-2xl">
+  <h1 className="text-5xl font-bold">
+    Code Anywhere. Run Anywhere.
+  </h1>
+  <p className="text-lg">
+  Write, run, and save code directly from your browser.
+</p>
+<Link
+  to="/register"
+  className="inline-block px-6 py-3 bg-black text-white rounded"
+>
+  Start Coding
+</Link>
+</div>
+</main>
+<section className="max-w-4xl mx-auto p-8">
+  <h2 className="text-3xl font-bold">
+    Why ICode?
+  </h2>
+</section>
     </>
   );
 }
