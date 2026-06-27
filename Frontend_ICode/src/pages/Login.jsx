3ecbbcce714426e4a4d92f99c58b69094534c5c1
Prashant Saud <saudprashant2062@gmail.com>
2026-06-27 11:26:59 +0530

    Made login submission

--- Frontend_ICode/src/pages/Login.jsx
+++ Frontend_ICode/src/pages/Login.jsx
@@ -5,6 +5,11 @@ import { useState } from "react";
 
 export default function Login() {
   const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  
+  function handleSubmit(e) {
+    e.preventDefault();
+  }
   return (
     <>
       <Navbar title="ICode" />
@@ -12,7 +17,7 @@ export default function Login() {
         <div className="w-full max-w-sm border border-green-500/25 bg-black p-12 shadow-[0_0_20px_rgba(61,220,132,0.03)]">
           <h1 className="text-4xl font-bold text-white border-b border-green-500/20 pb-3">Welcome back</h1>
           <p className="text-gray-400 mt-4 mb-8 border-l-2 border-green-500/15 pl-3">Sign in to your account</p>
-          <form className="flex flex-col gap-4">
+          <form className="flex flex-col gap-4" onsubmit={handleSubmit}>
             <input
               type="email"
               placeholder="Email"
@@ -23,7 +28,9 @@ export default function Login() {
             <input
               type="password"
               placeholder="Password"
-              className="w-full bg-gray-800 border border-green-500/25 px-4 py-3.5 text-gray-700 placeholder-gray-600 focus:outline-none focus:border-green-400 focus:ring-1 focus:ring-green-400"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+              className="w-full bg-gray-800 border border-green-500/25 px-4 py-3.5 text-gray-300 placeholder-gray-500 focus:outline-none focus:border-green-400 focus:ring-1 focus:ring-green-400"
             />
             <button className="w-full bg-green-500 text-black font-bold py-3.5 hover:bg-green-400 hover:shadow-lg hover:shadow-green-500/25 transition-all duration-200">
               Sign In
