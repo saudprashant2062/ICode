81fac95b6a8f479887780e6d11960fe0d368c958
Prashant Saud <saudprashant2062@gmail.com>
2026-06-30 23:44:41 +0530

    remove lang section

--- Frontend_ICode/src/context/ThemeContext.js
+++ Frontend_ICode/src/context/ThemeContext.js
@@ -1,3 +1,2 @@
 import { createContext } from "react";
-
 export const ThemeContext = createContext(null);
--- Frontend_ICode/src/pages/Landing.jsx
+++ Frontend_ICode/src/pages/Landing.jsx
@@ -11,7 +11,7 @@ const features = [
 ];
 
 const languages = [
-  "JavaScript", "Python", "Java", "C",
+  "JavaScript", "Python", "C",
 ];
 
 const year = new Date().getFullYear();
