5ca96e9355a5f75bb4d4a0ef01d99ae93a08d240
Prashant Saud <saudprashant2062@gmail.com>
2026-07-01 22:25:14 +0530

    added outlines for light mode

--- Frontend_ICode/src/components/FeatureCard.jsx
+++ Frontend_ICode/src/components/FeatureCard.jsx
@@ -1,6 +1,6 @@
 export default function FeatureCard({ title, description }) {
   return (
-    <div className="border border-[var(--clr-card-border)] p-5 bg-card hover:bg-green-500/5 hover:border-[var(--clr-card-border-hover)] transition-all duration-200">
+    <div className="border-2 border-[var(--clr-card-border)] p-5 bg-card shadow-[var(--clr-card-shadow)] hover:bg-green-500/5 hover:border-[var(--clr-card-border-hover)] transition-all duration-200">
       <h3 className="font-bold text-xl text-white">{title}</h3>
       <p className="text-gray-600 mt-1">{description}</p>
     </div>
